a7651c3f904e976355b6ff9ca9ed7f341ac54835
Zocateli <lzocateli00@outlook.com>
2023-10-03 21:42:18 -0300

    devcontainer

--- .devcontainer/custom-install.sh
+++ .devcontainer/custom-install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+USERSHELL=$(getent passwd $USER | cut -d: -f7)
 FILEZSH=$(which zsh)
 
-if [ ! -f "$FILEZSH" ]; then
+if [[ "/usr/bin/zsh" != "$USERSHELL" ]]; then
+    usermod -s $FILEZSH $USER
+fi
+
+FILEPOSH=$(which oh-my-posh)
+
+if [ ! -f "$FILEPOSH" ]; then
     curl -s https://ohmyposh.dev/install.sh | bash -s
     mv ~/.zshrc ~/.zshrc-old
     cp $TESTE/.devcontainer/.zshrc ~/.zshrc
 fi
 
-USERSHELL=$(getent passwd $USER | cut -d: -f7)
-
-if [[ "/bin/zsh" != "$USERSHELL" ]]; then
-    usermod -s $FILEZSH $USER
-fi
