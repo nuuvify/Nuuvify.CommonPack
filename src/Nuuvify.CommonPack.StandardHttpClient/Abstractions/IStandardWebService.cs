edb562c43ca4f5904ae755d978a0472cc0549406
lzocateli <lzocateli@outlook.com>
2024-03-21 17:04:59 -0300

    new webrequest

--- src/Nuuvify.CommonPack.StandardHttpClient/Abstractions/IStandardWebService.cs
+++ src/Nuuvify.CommonPack.StandardHttpClient/Abstractions/IStandardWebService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml;
 using Nuuvify.CommonPack.StandardHttpClient.Results;
@@ -17,7 +18,7 @@ namespace Nuuvify.CommonPack.StandardHttpClient
         Uri FullUrl { get; }
 
         /// <summary>
-        /// Cria uma nova instancia do HttpWebRequest
+        /// Cria uma nova instancia do HttpClient
         /// </summary>
         /// <param name="url">A url é obrigatorio</param>
         void CreateHttp(string url);
@@ -28,7 +29,15 @@ namespace Nuuvify.CommonPack.StandardHttpClient
         /// como Proxy e delegate para verificação de certificados.
         /// </summary>
         /// <param name="httpWebRequest"></param>
+        [Obsolete("WebRequest, HttpWebRequest, ServicePoint, and WebClient are obsolete. Use HttpClient instead.", DiagnosticId = "SYSLIB0014", UrlFormat = "https://aka.ms/dotnet-warnings/{0}")]
         void CreateHttp(HttpWebRequest httpWebRequest);
+        /// <summary>
+        /// Recebe uma instancia de HttpClientHandler onde você pode incluir outras configurações
+        /// como Proxy e delegate para verificação de certificados.
+        /// </summary>
+        /// <param name="httpClientHandler"></param>
+        /// <param name="uri"></param>
+        void CreateHttp(HttpClientHandler httpClientHandler, Uri uri);
 
         /// <summary>
         /// Executa o WebService, retornando o conteudo de forma padronizada
@@ -38,7 +47,29 @@ namespace Nuuvify.CommonPack.StandardHttpClient
         /// <param name="messageBody">Obrigatorio: XML contendo o SoapEnvelope</param>
         /// <param name="mediaType">Opcional: Tipo do conteudo da mensagem EX: "application/xml"</param>
         /// <returns></returns>
-        Task<HttpStandardReturn> RequestSoap(string urlRoute, StandardHttpMethods method, XmlDocument messageBody, string mediaType = "application/xml");
+        [Obsolete("Use o novo metodo RequestSoap")]
+        Task<HttpStandardReturn> RequestSoap(
+            string urlRoute,
+            StandardHttpMethods method,
+            XmlDocument messageBody,
+            string mediaType = "application/xml");
+
+
+        /// <summary>
+        /// Executa uma consulta SOAP
+        /// </summary>
+        /// <param name="urlRoute"></param>
+        /// <param name="messageBody"></param>
+        /// <param name="xmlDocumentmlResponseDocumentContains">Exemplo: XmlResponseDocumentContains</param>
+        /// <param name="xmlGetElementsByTagName">Exemplo: ns1:synDOCeDownloadXmlReturn</param>
+        /// <param name="mediaType"></param>
+        /// <returns></returns>
+        Task<HttpStandardReturn> RequestSoap(
+            string urlRoute,
+            XmlDocument messageBody,
+            string xmlDocumentmlResponseDocumentContains,
+            string xmlGetElementsByTagName,
+            string mediaType = "application/xml");
 
 
         /// <summary>
