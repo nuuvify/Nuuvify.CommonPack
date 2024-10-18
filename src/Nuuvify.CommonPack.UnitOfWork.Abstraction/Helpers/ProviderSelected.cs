940decc6811ce649ff0c6945589311896ea38518
Lincoln Zocateli <zocateli_lincoln@cat.com>
2024-10-18 20:14:49 -0300

    Implementado CancellationToken

--- src/Nuuvify.CommonPack.UnitOfWork.Abstraction/Helpers/ProviderSelected.cs
+++ src/Nuuvify.CommonPack.UnitOfWork.Abstraction/Helpers/ProviderSelected.cs
@@ -1,60 +1,60 @@
 ﻿using System.Text;
 
-namespace Nuuvify.CommonPack.UnitOfWork.Abstraction
+namespace Nuuvify.CommonPack.UnitOfWork.Abstraction;
+
+public static class ProviderSelected
 {
-    public static class ProviderSelected
+
+    public static readonly string Oracle = "Oracle";
+    public static readonly string Db2 = "Db2";
+    public static readonly string SqlServer = "SqlServer";
+    public static readonly string PostgreSQL = "PostgreSQL";
+    public static readonly string SqLite = "SqLite";
+    public static readonly string[] SuportedProviders = { Oracle, Db2, SqlServer, PostgreSQL, SqLite };
+
+
+    /// <summary>
+    /// Retorna uma string, separado por virgula, com todos os prividers suportados pelos metodos de extenção
+    /// </summary>
+    /// <returns></returns>
+    public static string GetSuportedProviders()
     {
+        var message = new StringBuilder();
 
-        public static readonly string Oracle = "Oracle";
-        public static readonly string Db2 = "Db2";
-        public static readonly string SqlServer = "SqlServer";
-        public static readonly string PostgreSQL = "PostgreSQL";
-        public static readonly string SqLite = "SqLite";
-        public static readonly string[] SuportedProviders = { Oracle, Db2, SqlServer, PostgreSQL, SqLite };
-
-
-        /// <summary>
-        /// Retorna uma string, separado por virgula, com todos os prividers suportados pelos metodos de extenção
-        /// </summary>
-        /// <returns></returns>
-        public static string GetSuportedProviders()
+        foreach (var item in SuportedProviders)
         {
-            var message = new StringBuilder();
-
-            foreach (var item in SuportedProviders)
-            {
-                message.Append($"{item},");
-            }
-
-            return message.ToString();
+            message.Append($"{item},");
         }
 
-        public static bool IsProviderOracle()
-        {
-            return ProviderName.Contains(Oracle);
-        }
-        public static bool IsProviderDb2()
-        {
-            return ProviderName.Contains(Db2) ||
-                ProviderName.Contains("IBM");
-        }
-        public static bool IsProviderSqlServer()
-        {
-            return ProviderName.Contains(SqlServer);
-        }
-        public static bool IsProviderPostgreSQL()
-        {
-            return ProviderName.Contains(PostgreSQL);
-        }
-        public static bool IsProviderSqLite()
-        {
-            return ProviderName.Contains(SqLite);
-        }
-
-        /// <summary>
-        /// Suported: <see cref="ProviderSelected.SuportedProviders" />
-        /// </summary>
-        public static string ProviderName { get; set; }
-
+        return message.ToString();
     }
+
+    public static bool IsProviderOracle()
+    {
+        return ProviderName.Contains(Oracle);
+    }
+    public static bool IsProviderDb2()
+    {
+        return ProviderName.Contains(Db2) ||
+            ProviderName.Contains("IBM");
+    }
+    public static bool IsProviderSqlServer()
+    {
+        return ProviderName.Contains(SqlServer);
+    }
+    public static bool IsProviderPostgreSQL()
+    {
+        return ProviderName.Contains(PostgreSQL);
+    }
+    public static bool IsProviderSqLite()
+    {
+        return ProviderName.Contains(SqLite);
+    }
+
+    /// <summary>
+    /// Suported: <see cref="ProviderSelected.SuportedProviders" />
+    /// </summary>
+    public static string ProviderName { get; set; }
+
 }
+
--- src/Nuuvify.CommonPack.UnitOfWork/Implementations/QueryablePageList.cs
+++ src/Nuuvify.CommonPack.UnitOfWork/Implementations/QueryablePageList.cs
@@ -1,35 +1,33 @@
-﻿using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿
 using Nuuvify.CommonPack.UnitOfWork.Abstraction.Interfaces;
 
-namespace Nuuvify.CommonPack.UnitOfWork
+namespace Nuuvify.CommonPack.UnitOfWork;
+
+public class QueryablePageList : IIQueryablePageList
 {
-    public class QueryablePageList : IIQueryablePageList
+    ///<inheritdoc/>
+    public IPagedList<T> ToPagedList<T>(
+        IQueryable<T> source,
+        int pageIndex,
+        int pageSize,
+        int indexFrom = 0)
     {
-        ///<inheritdoc/>
-        public IPagedList<T> ToPagedList<T>(
-            IQueryable<T> source, 
-            int pageIndex, 
-            int pageSize, 
-            int indexFrom = 0)
-        {
 
-            return source.ToPagedList<T>(pageIndex, pageSize, indexFrom);
-        }
+        return source.ToPagedList<T>(pageIndex, pageSize, indexFrom);
+    }
 
-        ///<inheritdoc/>
-        public async Task<IPagedList<T>> ToPagedListAsync<T>(
-            IQueryable<T> source, 
-            int pageIndex, 
-            int pageSize, 
-            int indexFrom = 0, 
-            CancellationToken cancellationToken = default)
-        {
+    ///<inheritdoc/>
+    public async Task<IPagedList<T>> ToPagedListAsync<T>(
+        IQueryable<T> source,
+        int pageIndex,
+        int pageSize,
+        int indexFrom = 0,
+        CancellationToken cancellationToken = default)
+    {
 
-            return await source.ToPagedListAsync<T>(pageIndex, pageSize, indexFrom, cancellationToken);
-
-        }
+        return await source.ToPagedListAsync<T>(pageIndex, pageSize, indexFrom, cancellationToken);
 
     }
+
 }
+
