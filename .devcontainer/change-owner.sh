c3abb9172598593f816cc7dbac3a35c84d9cc5f0
lzocateli <lzocateli@outlook.com>
2024-11-07 19:30:12 -0300

    DevContainer

--- .devcontainer/change-owner.sh
+++ .devcontainer/change-owner.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Mudar o proprietário das pastas montadas para o usuário do container
-chown -R devcontainer:devcontainer /home/devcontainer/.nuget
-chown -R devcontainer:devcontainer /home/devcontainer/.ssh
-chown -R devcontainer:devcontainer /home/devcontainer/.microsoft
-chown -R devcontainer:devcontainer /home/devcontainer/.config
+sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.nuget
+sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.ssh
+sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.microsoft
+sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.config
+sudo chown -R $HOST_USER:$HOST_USER $APP_PATH
+
+echo 'source /home/$HOST_USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /home/$HOST_USER/.zshrc
+echo 'eval "$(oh-my-posh init zsh --config /home/$HOST_USER/.config/atomic.omp.json)"' >> /home/$HOST_USER/.zshrc
