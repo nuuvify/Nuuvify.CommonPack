94b0fd8dcffaf3044071946ac884fadabf086d97
lzocateli <lzocateli@outlook.com>
2024-11-07 22:10:42 -0300

    devcontainer assim funciona com wsl

--- .devcontainer/change-owner.sh
+++ .devcontainer/change-owner.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Mudar o proprietário das pastas montadas para o usuário do container
-sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.nuget
-sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.ssh
-sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.microsoft
-sudo chown -R $HOST_USER:$HOST_USER /home/$HOST_USER/.config
-sudo chown -R $HOST_USER:$HOST_USER $APP_PATH
+# sudo chown -R $HOST_USER:$HOST_USER /root/.nuget
+# sudo chown -R $HOST_USER:$HOST_USER /root/.ssh
+# sudo chown -R $HOST_USER:$HOST_USER /root/.microsoft
+# sudo chown -R $HOST_USER:$HOST_USER /root/.config
+# sudo chown -R $HOST_USER:$HOST_USER $APP_PATH
 
-echo 'source /home/$HOST_USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /home/$HOST_USER/.zshrc
-echo 'eval "$(oh-my-posh init zsh --config /home/$HOST_USER/.config/atomic.omp.json)"' >> /home/$HOST_USER/.zshrc
+mkdir -p /root/.config
+cp $APP_PATH/.devcontainer/atomic.omp.json /root/.config/atomic.omp.json
+
+echo 'source /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /root/.zshrc
+echo 'eval "$(oh-my-posh init zsh --config /root/.config/atomic.omp.json)"' >> /root/.zshrc
